320694e25ba4b6740510bae4cf66c29ef09e92b3
Ranbabu <babu902654@gmail.com>
2025-11-16 13:50:53 +0530

    Add Google Desktop SERP configuration

---
+++ googleConfig.js
@@ -0,0 +1,10 @@
+export const googleDesktopSerpConfig = {
+  results: {
+    selector: "div.g",
+    fields: {
+      title: "h3",
+      link: "a@href",
+      description: "div.VwiC3b"
+    }
+  }
+};
